71a8b4c555cd0ba0fe4484bf89ac93c5ef3fc438
Yashraj Mishra <143256900+YashrajMishra6190@users.noreply.github.com>
2025-03-06 20:25:35 +0530

    Add files via upload

---
+++ dividend_calculator.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Streamlit UI
+st.title("Dividend Calculation")
+
+# User inputs
+x = st.number_input("Enter x in ₹:", min_value=0)
+y = st.number_input("Enter y in ₹:", min_value=0)
+month = st.number_input("Enter Month:", min_value=1, max_value=12)
+
+# Calculate the result if the month is valid
+if month > 12 or month < 1:
+    st.error("Invalid Month")
+else:
+    result = 0.075 * (x + y * month / 12)
+    st.write(f"Resultant for Dividend Calculation: ₹ {round(result)}")
